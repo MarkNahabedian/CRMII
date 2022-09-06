e5de423e4fac7077c9ddcce13d9d4ebe939597f8
MarkNahabedian <naha@mit.edu>
2022-09-05 21:05:09 -0400

    CSV escaping.  Better text collection.

--- src/nichols.jl
+++ src/nichols.jl
@@ -14,6 +14,20 @@ sheetnames(workbook)
 
 
 ############################################################
+# CSV file escaping:
+
+function rfc4180escape(str::AbstractString)
+    buf = IOBuffer()
+    s = split(str, '"')
+    for s1 in s
+        write(buf, '"')
+        write(buf, s1)
+        write(buf, '"')
+    end
+    String(take!(buf))
+end
+
+############################################################
 ### EXTRACTING THE DOCUMENT DESCRIPTIONS:
 
 # These files contain descriptive text for each document in the collection:
@@ -29,6 +43,17 @@ TEXT_DESCRIPTION_FILES =
 # We extract those descriptions into this Dict, keyed by document ID:
 DOCUMENT_DESCRIPTIONS = Dict{String, String}()
 
+function note_description!(id::AbstractString, text::AbstractString)
+    if haskey(DOCUMENT_DESCRIPTIONS, id)
+        if length(text) > 0
+            previous = DOCUMENT_DESCRIPTIONS[id]
+            DOCUMENT_DESCRIPTIONS[id] = previous * " " * strip(text)
+        end
+    else
+        DOCUMENT_DESCRIPTIONS[id] = strip(text)
+    end
+end
+
 # Regular expression for the input description lines:
 FILE_DESCRIPTION_REGEXP = r"^(?<id>[0-9]+-[0-9]+-[0-9]+)(?<desc>[^0-9].*)$"
 
@@ -40,6 +65,10 @@ function ignore_file_path(description_file_path)
     joinpath(d, b * "-ignored" * ext)
 end
 
+function extracted_descriptions_path()
+    joinpath(NICHOLS_DIR, "descriptions.tsv")
+end
+
 
 """
     read_text_descriptions(filepath::AbstractString)
@@ -49,13 +78,19 @@ Write unrecognized records to an "-ignored" file.
 function read_text_descriptions(filepath::AbstractString)
     line_number = 0
     open(ignore_file_path(filepath), "w") do ignored
+        previous_id = nothing
         for line in eachline(filepath)
             line_number += 1
             m = match(FILE_DESCRIPTION_REGEXP, line)
             if m == nothing
-                println(ignored, "$line_number\t$line")
+                if previous_id == nothing
+                    println(ignored, "$line_number\t$(rfc4180escape(line))")
+                else
+                    note_description!(previous_id, line)
+                end
             else
-                DOCUMENT_DESCRIPTIONS[m["id"]] = m["desc"]
+                note_description!(m["id"], m["desc"])
+                previous_id = m["id"]
             end
         end
     end
@@ -63,4 +98,14 @@ end
     
 # Extract the descriptions:
 map(read_text_descriptions, TEXT_DESCRIPTION_FILES)
+
+function write_descriptions()
+    open(extracted_descriptions_path(), "w") do io
+        for (id, text) in DOCUMENT_DESCRIPTIONS
+            println(io, "$id\t$(rfc4180escape(text))")
+        end
+    end
+end
+
+write_descriptions()
 
